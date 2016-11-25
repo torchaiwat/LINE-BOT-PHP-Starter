1be72bdd94d464bd06a55c711ef31908146d5400
torchaiwat <tor.test.api@gmail.com>
2016-11-25 14:49:16 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -39,9 +39,9 @@ if (!is_null($events['events'])) {
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 			$result = curl_exec($ch);
 			curl_close($ch);
-			echo "OK";
+
 			echo $result . "\r\n";
 		}
 	}
 }
-echo "OK";
+echo "Succeed";
