b507c6fcd760a520c716a180c01ceaf36b82b03e
torchaiwat <tor.test.api@gmail.com>
2016-11-25 14:27:10 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -30,6 +30,7 @@ if (!is_null($events['events'])) {
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+		}
 
 			$ch = curl_init($url);
 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
@@ -41,7 +42,7 @@ if (!is_null($events['events'])) {
 			curl_close($ch);
 
 			echo $result . "\r\n";
-		}
+		//}
 	}
 }
 echo "OK";
