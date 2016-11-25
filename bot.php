20c22e113849cc8b6df5af94612ee59b7a43d541
torchaiwat <tor.test.api@gmail.com>
2016-11-25 14:57:14 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -12,8 +12,7 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			//$text = $event['message']['text'];
-			$text = $event["OK"]['text'];
+			$text = $event['message']['text'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
